bef2ef3f7d49d4a966e7b7a648c4ff6178bd4d78
unknown <estavillofretz@gmail.com>
2023-06-04 10:27:10 +0800

    Add password generation code

---
+++ pass_gen.py
@@ -0,0 +1,13 @@
+import random
+
+number = int(input("Enter the number of passwords to generate: "))
+length = int(input("Enter the length of passwords to generate: "))
+
+
+char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789`~!@#$%^&*()-_+={}[]|\:;\"'<>,.?/"
+
+for _ in range(number):
+    password = ''
+    for _ in range(length):
+        password += random.choice(char) 
+    print(password)
